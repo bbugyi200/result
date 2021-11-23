0fc27c740960f79d7a2e10217543f2ca965b41ed
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-11-23 18:30:29 -0500

    Add __all__ to __init__.py file

--- src/result/__init__.py
+++ src/result/__init__.py
@@ -1,7 +1,9 @@
 """A fully type-annotated Rust-like Result type for Python."""
 
+from .result import Err, Ok, Result, return_lazy_result
+
+
+__all__ = ["Err", "Ok", "Result", "return_lazy_result"]
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
 __version__ = "0.2.0"
-
-from .result import Err, Ok, Result, return_lazy_result
